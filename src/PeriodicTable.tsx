413dcb41fc6314fd4b961800108a4444e018dd8e
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2021-01-06 18:23:23 -0800

    First commit

---
+++ src/PeriodicTable.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const PeriodicTable = () => {
+  return <h1>Hello World</h1>;
+};
+
+const hello = () => {
+  return (<p>This is a paragraph </p>)
+}
+
+export const Loafer = () => {
+  return (
+    hello()
+  )
+};
